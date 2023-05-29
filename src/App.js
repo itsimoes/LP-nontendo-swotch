eb83ef1ac5dc025e8586c2c6737e9769c632c7ce
Italo Simões <2d.italo@gmail.com>
2023-05-29 15:42:54 -0300

    atualização de deployment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar/Navbar';
-
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes> 
-      </Router>        
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,219 +1,61 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import MenuItem from "@mui/material/MenuItem";
-import { Link } from "react-router-dom";
+import React from "react";
 import "./style.scss";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import { Container } from "@mui/system";
+import "bootstrap/dist/css/bootstrap.css";
+import Carousel from "react-bootstrap/Carousel";
 
-import NintendoLogo from "../../assets/nintendo-logo.png";
+import Switch from "../../assets/switch.png";
+import SwitchLite from "../../assets/switch-lite.png";
+import SwitchOled from "../../assets/switch-oled.png";
+import SwitchLogo from "../../assets/switch-logo.png";
 
-const Navbar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
+const Home = () => {
   return (
-    <AppBar sx={{ background: "red" }} className="appbar">
+    <div className="wrapper">
       <Container>
-        <Toolbar disableGutters>
-          <Link
-            to="/"
-            style={{
-              textDecoration: "none",
-              color: "inherit",
-              display: "flex",
-            }}
-          >
-            <MenuItem>
-              {/* <AllInclusive
+        <div className="content-wrapper">
+          <div className="left-text">
+            <img className="switch-logo" src={SwitchLogo} alt=" " />
+            <h1>Nontendo Swotch</h1>
+            <p>
+              Avaiable now!
+              <br />
+              Starting from $299.99
+            </p>
+            <Box>
+              <Button
                 sx={{
-                  display: { xs: "none", md: "flex" },
-                  mr: 1,
+                  bgcolor: "white",
+                  color: "red",
+                  ":hover": { bgcolor: "tomato", color: "white" },
                 }}
-              /> */}
-              <Typography
-                variant="h6"
-                noWrap
-                sx={{
-                  display: { xs: "none", md: "flex" },
-                  fontFamily: "monospace",
-                  fontWeight: 700,
-                  letterSpacing: ".3rem",
-                  color: "inherit",
-                  textDecoration: "none",
-                }}
+                className="Button"
+                variant="contained"
               >
-                <img src={NintendoLogo} alt=" " className="nintendo-logo" />
-              </Typography>
-            </MenuItem>
-          </Link>
+                purchase now
+              </Button>
+            </Box>
+          </div>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              <MenuItem>
-                <Typography>
-                  <Link
-                    to="/"
-                    style={{
-                      textDecoration: "none",
-                      color: "red",
-                      display: "flex",
-                    }}
-                  >
-                    Home
-                  </Link>
-                </Typography>
-              </MenuItem>
-              <MenuItem>
-                <Typography>
-                  <Link
-                    to="/"
-                    style={{
-                      textDecoration: "none",
-                      color: "red",
-                      display: "flex",
-                    }}
-                  >
-                    About
-                  </Link>
-                </Typography>
-              </MenuItem>
-              <MenuItem>
-                <Typography>
-                  <Link
-                    to="/"
-                    style={{
-                      textDecoration: "none",
-                      color: "red",
-                      display: "flex",
-                    }}
-                  >
-                    Products
-                  </Link>
-                </Typography>
-              </MenuItem>
-              <MenuItem>
-                <Typography>
-                  <Link
-                    to="/"
-                    style={{
-                      textDecoration: "none",
-                      color: "red",
-                      display: "flex",
-                    }}
-                  >
-                    Contact
-                  </Link>
-                </Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-          <Link
-            to="/"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-              background: "#222",
-            }}
-          >
-            {/* <AllInclusive sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
-            <Typography
-              variant="h5"
-              noWrap
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-                background: "red",
-              }}
-            >
-              <img src={NintendoLogo} alt=" " className="nintendo-logo" />
-            </Typography>
-          </Link>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            <MenuItem sx={{ my: 2, color: "white", display: "block"}}>
-              <Typography sx={{ paddingRight: "10px", paddingLeft: "10px"}}>
-                <Link to="/" className="link">
-                  Home
-                </Link>
-              </Typography>
-            </MenuItem>
-            <MenuItem sx={{ my: 2, color: "white", display: "block" }}>
-              <Typography sx={{ paddingRight: "10px", paddingLeft: "10px"}}>
-                <Link to="/" className="link">
-                  About
-                </Link>
-              </Typography>
-            </MenuItem>
-            <MenuItem sx={{ my: 2, color: "white", display: "block" }}>
-              <Typography sx={{ paddingRight: "10px", paddingLeft: "10px"}}>
-                <Link to="/" className="link">
-                  Products
-                </Link>
-              </Typography>
-            </MenuItem>
-            <MenuItem sx={{ my: 2, color: "white", display: "block" }}>
-              <Typography sx={{ paddingRight: "10px", paddingLeft: "10px"}}>
-                <Link to="/" className="link">
-                  Contact
-                </Link>
-              </Typography>
-            </MenuItem>
-          </Box>
-        </Toolbar>
+          <div className="right-image">
+            <Carousel controls={false}>
+              <Carousel.Item interval={1500}>
+                <img className="switch" src={Switch} alt=" " />
+              </Carousel.Item>
+              <Carousel.Item interval={1500}>
+                <img className="switch" src={SwitchOled} alt=" " />
+              </Carousel.Item>
+              <Carousel.Item interval={1500}>
+                <img className="switch" src={SwitchLite} alt=" " />
+              </Carousel.Item>
+            </Carousel>
+          </div>
+        </div>
       </Container>
-    </AppBar>
+    </div>
   );
 };
 
-export default Navbar;
+export default Home;
